4a4c3fecd8758553fbf7eeefae29c82f733fc646
jtpark <vigabhan1231@gmail.com>
2022-05-07 10:47:46 +0900

    singleton blockchain

---
+++ blockchain/blockchain.go
@@ -0,0 +1,60 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
+type block struct {
+	Data string
+	Hash string
+	PrevHash string
+}
+
+type blockchain struct {
+	blocks []*block
+}
+
+// Singleton pattern
+var b *blockchain
+// 동기적으로 한 번만 실행하기 위해 사용하는 변수
+var once sync.Once
+
+func GetBlockchain() *blockchain {
+	if b == nil {
+		once.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis Block")
+ 		})
+	}
+	return b
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockchain().blocks)
+
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockchain().blocks[totalBlocks - 1].Hash
+}
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
+}
