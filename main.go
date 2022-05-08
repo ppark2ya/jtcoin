814492e95f30bee9959458f895c8ae28e87ed174
jtpark <vigabhan1231@gmail.com>
2022-05-08 21:02:58 +0900

    json encode marshal & using interface

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "github.com/ppark2ya/jtcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URL string
+
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+type URLDescription struct {
+	URL         URL    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"` // omitempty -> 만약 payload가 빈 값이면 아예 포함시키지 않는다. omitempty를 안쓰면 빈 스트링으로 초기화되서 return 함
+}
+
+// Stringers. java class에서 toString() method 구현하는 것과 비슷함.
+// func (u URLDescription) String() string {
+// 	return "Hello, I'm the URL Description"
+// }
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         URL("/"),
+			Method:      "GET",
+			Description: "See the documentation",
+		},
+		{
+			URL:         URL("/blocks"),
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	// b, err := json.Marshal(data)
+	// utils.HandleErr(err)
+	// fmt.Fprintf(rw, "%s", b)
+	json.NewEncoder(rw).Encode(data)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
